f78f03379377cbdddaff691bcf4111bb1592c700
Tronglich262 <lichtrong262@gmail.com>
2026-04-21 22:41:03 +0700

    cmt

--- Assets/Scripts/BackEnd/AcceptAllCertificates.cs
+++ Assets/Scripts/BackEnd/AcceptAllCertificates.cs
@@ -2,5 +2,12 @@ using UnityEngine.Networking;
 
 public class AcceptAllCertificates : CertificateHandler
 {
-    protected override bool ValidateCertificate(byte[] certificateData) => true;
+    protected override bool ValidateCertificate(byte[] certificateData)
+    {
+#if UNITY_EDITOR
+        return true;        // Chỉ bypass trong Editor
+#else
+        return false;       // Production thì kiểm tra bình thường
+#endif
+    }
 }
